3dfc02e2b2543cf0468fcd932b790d2d23f52091
Dave Mackie <calothian@gmail.com>
2014-02-25 14:03:47 -0800

    Second stage: add Mongo DB to back-end

--- hello.js
+++ hello.js
@@ -1,16 +1,37 @@
 // web.js
-var express = require("express");
 var logfmt = require("logfmt");
+var express = require("express");
+var mongo = require("mongodb");
+var monk = require("monk");
+
+var dbUri = process_env.MONGOHQ_URL;
+var db = monk(dbUri);
+
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  db.driver.admin.listDatabases(function(err, dbs) {
+    res.json(dbs);
+  });
+});
+
+app.get('/collections', function(req, res) {
+  db.driver.collectionNames(function(err, names) {
+    res.json(names);
+  });
+});
+
+app.get('/collections/:name', function(req, res) {
+  var collection = db.get(req.params.name);
+  collection.find({}, {limit:20}, function(err, docs) {
+    res.json(docs);
+  });
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Now listening on " + port);
 });
 
